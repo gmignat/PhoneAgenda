7d14a8d54f2a3806bcf4e20e5bb38958d9c36674
gmignat <gm_ignat@yahoo.com>
2022-07-10 08:52:23 +0300

    CreatingWebAPI

---
+++ SRC/PhoneAgenda/DataFillWebAPI/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataFillWebAPI
+{
+	public class Class1
+	{
+	}
+}
---
+++ SRC/PhoneAgenda/PhoneAgendaWebAPI5/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneAgendaWebAPI5.Controllers
+{
+	[ApiController]
+	[Route("[controller]/[Action]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> GetAllWeather()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+
+		[HttpGet]
+		public IEnumerable<int> GetData()
+		{
+			List<int> inturi = new List<int>();
+			inturi.Add(1);inturi.Add(4);
+			return inturi;
+
+		}
+	}
+}
